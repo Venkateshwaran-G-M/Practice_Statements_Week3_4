a6fcd8483bc964255c8143c8f55058a72b617396
Venkateshwaran-G-M <gurumoorthivenkateshwaran@gmail.com>
2026-04-02 15:32:50 +0530

    Q6

---
+++ RiskBinarySearch.java
@@ -0,0 +1,32 @@
+public class RiskBinarySearch {
+
+    static void findFloorCeil(int arr[], int target) {
+        int floor = -1, ceil = -1;
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == target) {
+                floor = ceil = arr[mid];
+                break;
+            }
+
+            if (arr[mid] < target) {
+                floor = arr[mid];
+                low = mid + 1;
+            } else {
+                ceil = arr[mid];
+                high = mid - 1;
+            }
+        }
+
+        System.out.println("Floor: " + floor);
+        System.out.println("Ceiling: " + ceil);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10, 25, 50, 100};
+        findFloorCeil(arr, 30);
+    }
+}
